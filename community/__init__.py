84cdfdff221ceb4e2a7b64c9bbd2c47cd3c673b8
DJ Figs <djfigs10@gmail.com>
2016-05-21 12:58:45 -0400

    Added Currency Data and updated __init__.py
    Provides Currency data for the four different currencies of TF2.

--- community/__init__.py
+++ community/__init__.py
@@ -1,2 +1,3 @@
 __author__ = 'djfigs1'
-from pyPackTF.community import community_prices
+from pyPackTF.community.community_prices import CommunityPrices
+from pyPackTF.community.currency_data import CurrencyData
