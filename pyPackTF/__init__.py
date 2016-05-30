9ca047736b836b56606fabeb5aed2d44184780b7
DJ Figs <djfigs10@gmail.com>
2016-05-30 15:05:21 -0400

    Preparing Community Prices
    Added some extra base code for community prices, and updated the
    __init__.py

--- pyPackTF/__init__.py
+++ pyPackTF/__init__.py
@@ -1,2 +1,14 @@
 __author__ = 'djfigs1'
+
+#BackpackTF Classes
 from pyPackTF.bpktf import UserRequester
+#TODO Import Classified Listings
+
+#Community Classes
+from pyPackTF.community import CommunityPrices
+from pyPackTF.community import CurrencyData
+#TODO Import Internal Items
+#TODO Import Price History
+
+#Market Classes
+from pyPackTF.market import MarketPrices
--- pyPackTF/community/community_prices.py
+++ pyPackTF/community/community_prices.py
@@ -2,30 +2,43 @@ __author__ = 'djfigs1'
 from pyPackTF import JSONRequester
 
 class CommunityPrices:
-    json = None
+    classJSON = None
 
-    def __init__(self, key):
+    def __init__(self, key, appid=440, since=None):
         self.key = key
         self.refreshPrices()
+        self.appid = appid
+        self.since = since
 
     def refreshPrices(self):
         #Note, it seems that with the Backpack.TF API you can only send a request every five minutes or so.
         url = "http://backpack.tf/api/IGetPrices/v4/?key=" + self.key
-        self.json = JSONRequester.requestJSON(url)
+        self.classJSON = JSONRequester.requestJSON(url)
 
     #Price Functions
     def isResponseSuccessful(self):
-        success = self.json['response']['success']
+        success = self.classJSON['response']['success']
         if success == 1:
             return True
         else:
             return False
 
-
     def getMessage(self):
-        message = self.json['response']['message']
+        message = self.classJSON['response']['message']
         return message
 
     def getCurrentTime(self):
-        message = self.json['response']['current_time']
+        current_time = self.classJSON['response']['current_time']
         return current_time
+
+class CommunityItem:
+    def __init__(self, item, itemJSON):
+        self.itemJSON = itemJSON
+        self.item = item
+
+    def getName(self):
+        return self.item
+
+    def getDefIndexes(self):
+        return self.itemJSON['defindex']
+
