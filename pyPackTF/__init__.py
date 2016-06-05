ebf821da851ac42154dc3520e7f610b22e0f60d5
DJ Figs <djfigs10@gmail.com>
2016-06-05 19:10:45 -0400

    Changed Init Files and Community Prices
    Added extra functions to Community prices to get prices of an index.

--- pyPackTF/__init__.py
+++ pyPackTF/__init__.py
@@ -1,14 +1,14 @@
 __author__ = 'djfigs1'
 
 #BackpackTF Classes
-from pyPackTF.bpktf import UserRequester
+from pyPackTF.bpktf.users import UserRequester
 #TODO Import Classified Listings
 
 #Community Classes
-from pyPackTF.community import CommunityPrices
-from pyPackTF.community import CurrencyData
+from pyPackTF.community.community_prices import CommunityPrices
+from pyPackTF.community.currency_data import CurrencyData
 #TODO Import Internal Items
 #TODO Import Price History
 
 #Market Classes
-from pyPackTF.market import MarketPrices
+from pyPackTF.market.market_prices import MarketPrices
