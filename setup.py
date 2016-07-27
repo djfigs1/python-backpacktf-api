945f27784d42733067c9baf1d371d4a5327d216a
DJ Figs <djfigs10@gmail.com>
2016-07-27 17:00:21 -0400

    Fixing Tests

--- setup.py
+++ setup.py
@@ -4,4 +4,7 @@ setup(name='pyPackTF',
       description="Unofficial Python API to commuincate with the Backpack.TF API",
       long_description=open('README.md').read(),
       url="https://github.com/djfigs1/pyPackTF",
-      packages=["pyPackTF", "pyPackTF.bpktf", "pyPackTF.community", "pyPackTF.market", "pyPackTF.tests"])
+      packages=["pyPackTF", "pyPackTF.bpktf", "pyPackTF.community", "pyPackTF.market", "pyPackTF.tests"],
+      test_suite='nose.collector',
+      tests_require=['nose', 'nose-cover3'],
+      )
