885b422bf1c9348153933ada9ca0e2410410859a
DJ Figs <djfigs10@gmail.com>
2016-07-27 16:50:45 -0400

    Updated Tests and Setup

--- pyPackTF/tests/offline_tests.py
+++ pyPackTF/tests/offline_tests.py
@@ -6,7 +6,7 @@ class CommunityPricesTests(unittest.TestCase):
         from pyPackTF import CommunityPrices
 
         CP = CommunityPrices("N/A", offline=True)
-        json_file = open('sample_json/community_prices.json', 'r')
+        json_file = open('community_prices.json', 'r')
         file_json = json.load(json_file)
         CP.loadJSON(file_json)
 
@@ -16,21 +16,23 @@ class CommunityPricesTests(unittest.TestCase):
         self.assertEqual(self.CP.getCurrentTime(), 1469643121)
 
     def test_IsValid(self):
-        from pyPackTF import CommunityPrices
-
-        CP = CommunityPrices("N/A", offline=True)
-        json_file = open('sample_json/community_prices.json', 'r')
-        file_json = json.load(json_file)
-        CP.loadJSON(file_json)
-
         self.assertEqual(self.CP.isResponseSuccessful(), True)
 
-    def test_FailureMessage(self):
+    def test_NoFailureMessage(self):
         from pyPackTF import Exceptions
 
         with self.assertRaises(Exceptions.NoErrorMessage):
             self.CP.getMessage()
 
+    def test_FailureMessage(self):
+        from pyPackTF import CommunityPrices
+
+        CP = CommunityPrices("N/A", offline=True)
+        json_file = open('failed_request.json', 'r')
+        file_json = json.load(json_file)
+        CP.loadJSON(file_json)
+
+        self.assertEqual(type(CP.getMessage()), str)
     def test_Item_Price(self):
         test_item = self.CP.getCommunityItems()[0]
 
@@ -41,25 +43,25 @@ class CommunityPricesTests(unittest.TestCase):
         test_item = self.CP.getCommunityItems()[0]
         qualities = test_item.getQualities()
 
-        self.assertEqual(len(qualities), 1)
+        self.assertEqual(len(qualities) >= 1, True)
 
     def test_Item_Defindex(self):
         test_item = self.CP.getCommunityItems()[0]
         defindex = test_item.getDefIndexes()
 
-        self.assertEqual(defindex[0], 781)
+        self.assertEqual(type(defindex[0]), int)
 
     def test_Item_Difference(self):
         test_item = self.CP.getCommunityItems()[0]
         difference = test_item.getDifference(6)
 
-        self.assertEqual(difference, -0.34)
+        self.assertEqual(type(difference), float)
 
     def test_Item_LastUpdated(self):
         test_item = self.CP.getCommunityItems()[0]
         last_updated = test_item.getLastUpdated(6)
 
-        self.assertEqual(last_updated, 1459527840)
+        self.assertEqual(type(last_updated), int)
 
     def test_Item_Currency(self):
         test_item = self.CP.getCommunityItems()[0]
--- setup.py
+++ setup.py
@@ -4,4 +4,4 @@ setup(name='pyPackTF',
       description="Unofficial Python API to commuincate with the Backpack.TF API",
       long_description=open('README.md').read(),
       url="https://github.com/djfigs1/pyPackTF",
-      packages=["pyPackTF"])
+      packages=["pyPackTF", "pyPackTF.bpktf", "pyPackTF.community", "pyPackTF.market", "pyPackTF.tests"])
